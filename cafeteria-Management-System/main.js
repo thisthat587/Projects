bdea5c2d42a564327868b323773899a963e12218
Piyush singh <thisthat587@gmail.com>
2024-01-31 14:53:51 +0530

    .

--- cafeteria-Management-System/main.js
+++ cafeteria-Management-System/main.js
@@ -3,9 +3,6 @@ const say = require("say");
 
 function createWindow()
 {
-  // const text = 'welcome';
-
-  // Get the size of the primary display
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   const mainWindow = new BrowserWindow({
@@ -17,16 +14,9 @@ function createWindow()
       webSecurity: false,
     },
   });
-  // say.speak(text);
   mainWindow.setMenuBarVisibility(false);
   mainWindow.loadFile("main.html");
 
-  mainWindow.webContents.once('did-finish-load', () =>
-  {
-    // Speak the welcome message after the window is loaded
-    const text = 'Welcome';
-    say.speak(text, 'espeak', 1.0, null, null, { espeak: { amplitude: 100, wordgap: 0, pitch: 50 } });
-  });
 }
 
 app.whenReady().then(() =>
